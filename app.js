570f9f85ab2c5f8ea8eebe71e0ec8fc81220404c
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-02-16 13:22:14 -0500

    minor changes to app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const userRouter = require('./user.controller');
-const db = require('./db.js');
 
 
 const app = express();
@@ -13,31 +12,6 @@ app.use('/v1/user',userRouter);
 app.get('/healthz', (req,res) => {
     res.sendStatus(200)
   });
-
-
-// function auth (req, res) {
-//     var authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         var err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');
-//         res.sendStatus(401);
-//         return;
-//     }
-  
-//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-//     var user = auth[0];
-//     var pass = auth[1];
-//     if (user == 'admin' && pass == 'password') {
-//          console.log('authorized');
-//     } else {
-//         var err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');      
-//         res.sendStatus(401);
-//         return;
-//     }
-//   }
-  
-//   app.use(auth);
 
   app.get('*', (req,res) => {
     res.sendStatus(404)
