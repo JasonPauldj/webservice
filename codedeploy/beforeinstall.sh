ec454f7f219a90a7524b41ba5c9989a8651de4af
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-03-28 22:18:52 -0400

    changed paths in appspec

--- codedeploy/beforeinstall.sh
+++ codedeploy/beforeinstall.sh
@@ -6,3 +6,6 @@ sudo systemctl stop webservice.service
 #removing previously installed app folder. the app folder itself gets deleted
 sudo rm -rf /home/ec2-user/webservice.zip
 sudo rm -rf /home/ec2-user/app
+
+#creating app folder
+cd /home/ec2-user && mkdir app
