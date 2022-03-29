40d604a78f23cc8d0ce7580a7e8b9be2e4955c03
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-03-29 15:26:01 -0400

    cleaning up afterinstall.sh

--- codedeploy/afterinstall.sh
+++ codedeploy/afterinstall.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# cd /home/ec2-user && unzip webservice.zip -d /home/ec2-user/app
-sudo cd /home/ec2-user/app
-echo "printing pwd"
-pwd
-echo "listing before changing permissions"
-ls -l
+# cd /home/ec2-user && changing permission and installing node packages.
 cd /home/ec2-user/app 
 sudo chown ec2-user *
-echo "listing after changing permissions"
-ls -l
-
 cd /home/ec2-user/app && sudo npm i --only=prod
