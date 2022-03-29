a3923c72473df84b7dd2e70f85173867d0346cce
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-03-29 11:03:22 -0400

    debugging assignement7

--- codedeploy/afterinstall.sh
+++ codedeploy/afterinstall.sh
@@ -2,5 +2,11 @@
 
 # cd /home/ec2-user && unzip webservice.zip -d /home/ec2-user/app
 sudo cd /home/ec2-user/app
+echo "printing pwd"
+pwd
+echo "listing before changing permissions"
+ls -l
 sudo chown ec2-user *
+echo "listing after changing permissions"
+ls -l
 cd /home/ec2-user/app && npm i --only=prod
