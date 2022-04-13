0c87232854754cc99e0f9b40f2b2da84130c4652
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-04-12 20:33:02 -0400

    removed comments

--- app.js
+++ app.js
@@ -34,13 +34,6 @@ app.use('/v1/verifyUserEmail', async (req, res)=>{
    logger.info("Fetching item from DynamoDB");
    const tableName = process.env.DYNAMODB_TABLE_NAME;
    logger.info("TableName " + tableName);
-  //  const dynamoInputGetParams = {
-  //      Key: {
-  //          userId : {S:email}
-  //         },
-  //     TableName: tableName
-  //  }
-  // const dynamoCommand = new GetItemCommand(dynamoInputParams);
   const currentTime = Math.round(new Date().getTime()/1000);
   const dynamoQueryInputParams ={
       KeyConditionExpression: "userId = :uId",
@@ -57,24 +50,6 @@ app.use('/v1/verifyUserEmail', async (req, res)=>{
   logger.info(dynamoQueryInputParams);
    const dynamoCommand = new QueryCommand(dynamoQueryInputParams);
    try {
-     // const dynamoResponse = await dynamodbClient.send(dynamoCommand);
-      // '$metadata': {
-      //     httpStatusCode: 200,
-      //     requestId: 'RQGRQG66Q081EAP9T857HUU967VV4KQNSO5AEMVJF66Q9ASUAAJG',
-      //     extendedRequestId: undefined,
-      //     cfId: undefined,
-      //     attempts: 1,
-      //     totalRetryDelay: 0
-      //   },
-      //   ConsumedCapacity: undefined,
-      //   Item: {
-      //     ttl: { N: '1649740990' },
-      //     token: {
-      //       S: '53e67b93be677e00799663d13d1be2b87023dba7f88120312a31b013290fc032e3a47882720ade2b375aef0bf77e534d160a43332faa199c3594d099f020816f'
-      //     },
-      //     userId: { S: 'jane24.doe@example.com' }
-      //   }
-      // }
       
        const dynamoResponse = await dynamodbClient.send(dynamoCommand);
       //if couldn't find a record
